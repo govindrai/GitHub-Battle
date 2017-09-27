e0637b698944fc6719c2f4635b39d8a9fcf23cf9
Govind Rai <raigovind93@gmail.com>
2017-09-27 00:03:02 -0700

    Configure webpack and create initial project structure

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// const path = require("path");
+const path = require("path"),
+  HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
