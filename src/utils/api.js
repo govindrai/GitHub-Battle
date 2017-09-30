7c88f4773fd559ceac4106f843c3466e966bed2b
Govind Rai <raigovind93@gmail.com>
2017-09-30 08:32:21 -0700

    Create API for communicating with GitHub API and retrieveing user data

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,64 @@
 import axios from "axios";
 
+// var id = "YOUR_CLIENT_ID";
+// var sec = "YOUR_SECRET_ID";
+// var params = "?client_id=" + id + "&client_secret=" + sec;
+
+function getProfile(username) {
+  return axios
+    .get("https://api.github.com/users/" + username)
+    .then(function(user) {
+      return user.data;
+    });
+}
+
+function getRepos(username) {
+  return axios.get(
+    "https://api.github.com/users/" + username + "/repos" + "?per_page=100"
+  );
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore(profile, repos) {
+  var followers = profile.followers;
+  var totalStars = getStarCount(repos);
+
+  return followers * 3 + totalStars;
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([getProfile(player), getRepos(player)]).then(data => {
+    var profile = data[0];
+    var repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    };
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+const battle = players => {
+  return axios
+    .all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError);
+};
+
 const fetchPopularRepos = language => {
   const encodedURI = window.encodeURI(
     "https://api.github.com/search/repositories?q=stars:>1+language:" +
@@ -10,4 +69,4 @@ const fetchPopularRepos = language => {
   return axios.get(encodedURI).then(response => response.data.items);
 };
 
-export default { fetchPopularRepos };
+export default { battle, fetchPopularRepos };
